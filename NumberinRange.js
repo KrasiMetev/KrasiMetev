7d94d5d84465a2d1b52391a16b64669aa8a97e55
Krasimir Metev <98229045+KrasiMetev@users.noreply.github.com>
2022-05-28 14:01:55 +0300

    Delete NumberinRange.js

--- NumberinRange.js
+++
@@ -1,9 +0,0 @@
-function numberInRange(input) {
-    let a = Number(input[0]);
-     if (a >= -100 & a <= 100 & a != 0) {
-         console.log("Yes");
-     } else {
-         console.log("No")
-     }
-}
-numberInRange(["500"])
